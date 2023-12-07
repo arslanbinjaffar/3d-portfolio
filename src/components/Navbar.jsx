c423e0e993ea39a3203286ca5121970e078f9cdc
Arslan jafar <arslanbinjaffar12000@gmail.com>
2023-12-07 15:08:25 +0500

    updated 3d portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+import { logo } from "../assets/images";
+
+const Navbar = () => {
+  return (
+    <header className='header'>
+      <NavLink to='/'>
+        {/* <img src={logo} alt='logo' className='w-18 h-18 object-contain' /> */}
+        <p className="bg-white w-18 h-18 p-2 rounded-lg text-lg font-bold text-blue-400"> Arslan</p>
+      </NavLink>
+      <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+          About
+        </NavLink>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
